a8b09101376cbbfe6a40a6809af30267563af6f8
Nick Jackson <nickdjackson@msn.com>
2023-02-04 21:54:42 -0500

    upload functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'g24g3qg53q5gq35q5h4'
@@ -22,9 +23,16 @@ def your_url():
 
         if request.form['code'] in urls.keys():
             flash('That short name has already been taken. Please select another name.')
-            return redirect(url_for('home'))    
+            return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url':request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/nickdjackson/url-shortener/' + full_name)
+            urls[request.form['code']] = {'file':full_name}
+        
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
