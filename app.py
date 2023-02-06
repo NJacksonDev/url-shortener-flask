87e57a681080e5c1a9eb3d9b3a717489662ec769
Nick Jackson <nickdjackson@msn.com>
2023-02-05 21:12:11 -0500

    file route

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/Users/nickdjackson/url-shortener/' + full_name)
+            f.save('/Users/nickdjackson/url-shortener/static/user_files/' + full_name)
             urls[request.form['code']] = {'file':full_name}
         
         with open('urls.json', 'w') as url_file:
@@ -48,3 +48,7 @@ def redirect_to_url(code):
             if code in urls.keys():
                 if 'url' in urls[code].keys():
                     return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+
+
